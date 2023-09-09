b44165b635e53f360d6983ed699b17dfd88a556f
Patrício Andrade <tricioandrade@outlook.com>
2023-09-09 18:35:27 +0100

    :sparkles: php unity GenerateKeysTest class created

---
+++ tests/GenerateKeysTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use OpensCrypt\App\Core\GenerateKeys;
+use PHPUnit\Framework\TestCase;
+
+
+final class GenerateKeysTest extends TestCase
+{
+    public GenerateKeys $generateKeys;
+
+    public function setUp(): void
+    {
+        $this->generateKeys = new GenerateKeys();
+    }
+
+    public function generateKeys()
+    {
+        $this->generateKeys();
+    }
+    
+}
